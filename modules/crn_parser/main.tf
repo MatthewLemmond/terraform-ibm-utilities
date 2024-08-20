b695adce48116685282c0292213c5e5635585ada
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-08-20 19:33:22 -0400

    feat: add crn parser that rejects invalid crn input

--- modules/crn_parser/main.tf
+++ modules/crn_parser/main.tf
@@ -4,11 +4,11 @@ This file is used to implement the ROOT module.
 
 locals {
   split_crn        = split(":", var.crn)
-  ctype            = length(local.split_crn) >= 4 ? local.split_crn[3] : null
-  service_name     = length(local.split_crn) >= 5 ? local.split_crn[4] : null
-  region           = length(local.split_crn) >= 6 ? local.split_crn[5] : null
-  scope            = length(local.split_crn) >= 7 ? local.split_crn[6] : null
-  service_instance = length(local.split_crn) >= 8 ? local.split_crn[7] : null
-  resource_type    = length(local.split_crn) >= 9 ? local.split_crn[8] : null
-  resource         = length(local.split_crn) >= 10 ? local.split_crn[9] : null
+  ctype            = local.split_crn[3]
+  service_name     = local.split_crn[4]
+  region           = local.split_crn[5]
+  scope            = local.split_crn[6]
+  service_instance = local.split_crn[7]
+  resource_type    = local.split_crn[8]
+  resource         = local.split_crn[9]
 }
--- modules/crn_parser/variables.tf
+++ modules/crn_parser/variables.tf
@@ -5,4 +5,9 @@
 variable "crn" {
   type        = string
   description = "The CRN to be parsed."
+
+  validation {
+    condition     = length(split(":", var.crn)) == 10
+    error_message = "CRN is invalid, for details see https://cloud.ibm.com/docs/account?topic=account-crn."
+  }
 }
